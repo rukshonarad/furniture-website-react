ae2f515fb8b6bf77b7acf3d6498f5750c4b6b1e2
Rukshona Rad <rukshona.rad@gmail.com>
2023-12-06 11:30:38 -0500

    Add progress section

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,14 @@
 import { TopNavigation } from "./sections/top-navigation/Top-navigation";
 import { Hero } from "./sections/hero/Hero";
 import { Service } from "./sections/service/Service";
+import { Progress } from "./sections/progress/Progress";
 const Home = () => {
     return (
         <>
             <TopNavigation />
             <Hero />
             <Service />
+            <Progress />
         </>
     );
 };
