4da57530d63d946fcc1532b651d31adcf8d3889e
Rukshona Rad <rukshona.rad@gmail.com>
2023-12-06 11:07:36 -0500

    Add Service section

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,12 @@
 import { TopNavigation } from "./sections/top-navigation/Top-navigation";
 import { Hero } from "./sections/hero/Hero";
+import { Service } from "./sections/service/Service";
 const Home = () => {
     return (
         <>
             <TopNavigation />
             <Hero />
+            <Service />
         </>
     );
 };
