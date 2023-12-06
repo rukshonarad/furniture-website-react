768af6f6370c1dd2ae2e9a6eea10d9ffc5bc9ac4
Rukshona Rad <rukshona.rad@gmail.com>
2023-12-06 11:20:03 -0500

    Add designe section

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,14 @@
 import { TopNavigation } from "./sections/top-navigation/Top-navigation";
 import { Hero } from "./sections/hero/Hero";
 import { Service } from "./sections/service/Service";
+import { Designe } from "./sections/designe/Designe";
 const Home = () => {
     return (
         <>
             <TopNavigation />
             <Hero />
             <Service />
+            <Designe />
         </>
     );
 };
