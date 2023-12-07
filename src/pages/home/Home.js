423cf36ac8af4cb9f84b8fa06da4d001322f14af
Rukshona Rad <rukshona.rad@gmail.com>
2023-12-06 23:11:57 -0500

    Create rresable component with prop for Progress section

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,7 +2,7 @@ import { TopNavigation } from "./sections/top-navigation/Top-navigation";
 import { Hero } from "./sections/hero/Hero";
 import { Services } from "./sections/service/Service";
 import { Designe } from "./sections/designe/Designe";
-import { Progress } from "./sections/progress/Progress";
+import { Progresses } from "./sections/progress/Progress";
 import { Product } from "./sections/product/Product";
 import { Customers } from "./sections/customers/Customers";
 import { Subscribe } from "./sections/subscribe/Subscribe";
@@ -14,7 +14,7 @@ const Home = () => {
             <Hero />
             <Services />
             <Designe />
-            <Progress />
+            <Progresses />
             <Product />
             <Customers />
             <Subscribe />
