81cf750979953b55909ae7ce8525d9ab6602f25e
Rukshona Rad <rukshona.rad@gmail.com>
2023-12-06 19:59:11 -0500

    Change navigation li tags to reusable components

--- src/pages/home/sections/top-navigation/Top-navigation.js
+++ src/pages/home/sections/top-navigation/Top-navigation.js
@@ -1,6 +1,13 @@
 import "./Top-navigation.css";
 import logo from "../../../../assets/logo.svg";
 import menu from "../../../../assets/menu.svg";
+const links = [
+    { text: "Product", link: "https://google.com" },
+    { text: "Rooms", link: "https://google.com" },
+    { text: "Inspiration", link: "https://facebook.com" },
+    { text: "Support", link: "https://google.com" },
+    { text: "Sign in", link: "https://google.com" }
+];
 const TopNavigation = () => {
     return (
         <header class="header">
@@ -8,22 +15,14 @@ const TopNavigation = () => {
                 <img src={logo} alt="Whiter logo" class="logo" />
                 <nav class="nav inline-block">
                     <ul>
-                        <li>
-                            <a href="">Products</a>
-                        </li>
-                        <li>
-                            <a href="">Rooms</a>
-                        </li>
-                        <li>
-                            <a href="">Inspiration</a>
-                        </li>
-                        <li>
-                            <a href="">Support</a>
-                        </li>
+                        {links.map((link, idx) => {
+                            return (
+                                <li key={idx}>
+                                    <a href={link.link}>{link.text}</a>
+                                </li>
+                            );
+                        })}
                     </ul>
-                    <li class="last-nav">
-                        <a href="">Sign in</a>
-                    </li>
                 </nav>
                 <img src={menu} alt="" id="mobile-nav-icon" />
                 <nav class="mobile-nav">
