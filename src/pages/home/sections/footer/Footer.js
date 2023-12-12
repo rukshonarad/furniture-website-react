0332a0126f97187740ef0313fa07d5d66b23b4e4
Rukshona Rad <rukshona.rad@gmail.com>
2023-12-11 22:57:22 -0500

    Finish footer reusable comp

--- src/pages/home/sections/footer/Footer.js
+++ src/pages/home/sections/footer/Footer.js
@@ -1,45 +1,55 @@
 import "./Footer.css";
-const Footer = () => {
+import logo from "../../../../assets/logo.svg";
+import { contactLinks, productLinks, aboutUsLinks } from "./data.js";
+const Link = (props) => {
+    return (
+        <a href={props.link} className="text-lg">
+            {props.text}
+        </a>
+    );
+};
+export const Footer = () => {
     return (
         <footer>
-            <div className="footer container">
-                <div className="footer__content">
-                    <div className="footer-card__main">
-                        <div className="footer-logo">
-                            <img src="./images/logo.svg" alt="" />
-                        </div>
-                        <p className="text-small footer-text">
-                            We make furniture with love and passions. No doubt,
-                            really.
-                        </p>
-                        <p className="text-small footer-text">
-                            © TuranTech Inc.
-                        </p>
+            <div className="footer__content">
+                <div className="footer-card__main">
+                    <div className="footer-logo">
+                        <img src={logo} alt="" />
                     </div>
+                    <p className="text-small footer-text">
+                        We make furniture with love and passions. No doubt,
+                        really.
+                    </p>
+                    <p className="text-small footer-text">© TuranTech Inc.</p>
+                </div>
 
-                    <div className="footer-card">
-                        <h4 className="text-medium name">CONTACT</h4>
-                        <a>62 Orp St, Seattle, USA</a>
-                        <a href="mailto">sales@furnatur.com</a>
-                        <a href="tel:347-567-2828" className="footer__text">
-                            +1 800 726 915273
-                        </a>
-                    </div>
-                    <div className="footer-card">
-                        <h4 className="text-medium name">PRODUCT</h4>
-                        <a href="">Living Room</a>
-                        <a href="">Bed Room</a>
-                        <a href="">Office Room</a>
-                    </div>
-                    <div className="footer-card">
-                        <h4 className="text-medium name">OUR COMPANY</h4>
-                        <a href="">About Us</a>
-                        <a href="">Service</a>
-                        <a href="">products</a>
-                    </div>
+                <div className="footer-card">
+                    <h4 className="text-medium name">CONTACT</h4>
+                    <a>62 Orp St, Seattle, USA</a>
+                    {contactLinks.map((link, idx) => {
+                        return (
+                            <Link key={idx} link={link.link} text={link.text} />
+                        );
+                    })}
+                </div>
+                <div className="footer-card">
+                    <h4 className="text-medium name">PRODUCT</h4>
+                    {productLinks.map((link, idx) => {
+                        return (
+                            <Link key={idx} link={link.link} text={link.text} />
+                        );
+                    })}
+                </div>
+                <div className="footer-card">
+                    <h4 className="text-medium name">OUR COMPANY</h4>
+
+                    {aboutUsLinks.map((link, idx) => {
+                        return (
+                            <Link key={idx} link={link.link} text={link.text} />
+                        );
+                    })}
                 </div>
             </div>
         </footer>
     );
 };
-export { Footer };
